8b02dcb0807635d4e313d22a153d5526dd4c92a6
Piotr Adamczyk <xeno@xeno.yt>
2019-02-16 11:47:31 -0700

    a few basic tests

--- test/global_trade_system.js
+++ test/global_trade_system.js
@@ -1,7 +1,34 @@
-contract('GlobalTradeSystem', function(accounts) {
-  it("should assert true", function(done) {
-    var global_trade_system = GlobalTradeSystem.deployed();
-    assert.isTrue(true);
-    done();
+var GlobalTradeSystem = artifacts.require("GlobalTradeSystem");
+
+contract("GlobalTradeSystem", function(accounts) {
+  it("should assign assets correctly", async function() {
+    var GTS = await GlobalTradeSystem.deployed();
+    const emitter = accounts[0];
+    const receiver = accounts[1];
+    const data = "0x" + Math.floor(Math.random() * 2000);
+    await GTS.assign(receiver, data, { from: emitter });
+    const assigned = await GTS.getAsset(1);
+    assert.equal(assigned.emitter, emitter, "Emitter is not equal");
+    assert.equal(assigned.emitter, emitter, "Receiver is not equal");
+    assert.equal(assigned.emitter, emitter, "Data is not equal");
+  });
+  it("should burn assets correctly", async function() {
+    var GTS = await GlobalTradeSystem.deployed();
+    const emitter = accounts[0];
+    const receiver = accounts[1];
+    const data = "0xabcd";
+    await GTS.assign(receiver, data, { from: emitter });
+    await GTS.burn(1, { from: emitter });
+  });
+  it("should throw when burning a someone else's asset", async function() {
+    var GTS = await GlobalTradeSystem.deployed();
+    const emitter = accounts[0];
+    const receiver = accounts[1];
+    const data = "0xabcd";
+    await GTS.assign(receiver, data, { from: emitter });
+    try {
+      await GTS.burn(1, { from: accounts[3] });
+      assert.fail("Burn did not throw");
+    } catch (err) {}
   });
 });
